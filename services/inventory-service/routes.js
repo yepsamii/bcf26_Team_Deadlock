98d4f652a01b95595a19fdc96f2a4ac883ef5f1d
Ridwan414 <u2008039@student.cuet.ac.bd>
2026-01-29 15:24:46 +0600

    fix log location

--- services/inventory-service/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const inventoryRouter = express.Router();
-
-inventoryRouter.get('/', (req, res) => {
-    res.status(200).json({ message: 'Some inventory data !' });
-});
-
-module.exports = inventoryRouter;
